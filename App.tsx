0388f0a7ff24b8b90da6bba66cf19da38e695cb3
khaymanii <kennethakpo61@gmail.com>
2025-10-11 11:25:03 +0100

    feat: added fonts

--- App.tsx
+++ App.tsx
@@ -1,9 +1,33 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
 import "./global.css";
 import Login from "./src/screens/Login";
+import {
+  useFonts,
+  Merriweather_400Regular,
+  Merriweather_700Bold,
+  Merriweather_500Medium,
+} from "@expo-google-fonts/merriweather";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Merriweather_400Regular,
+    Merriweather_500Medium,
+    Merriweather_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return null; // You can also show a loading screen here
+  }
+
+  const AppText: React.FC<React.ComponentProps<typeof Text>> = (props) => (
+    <Text
+      {...props}
+      style={[{ fontFamily: "Merriweather_400Regular" }, props.style]}
+    >
+      {props.children}
+    </Text>
+  );
   return (
     <View style={styles.container}>
       <Login /> <StatusBar style="auto" />
