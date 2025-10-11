4039a6050774967c013e4212abfb350b63e75480
khaymanii <kennethakpo61@gmail.com>
2025-10-11 09:03:20 +0100

    fix

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import "./global.css";
+import SplashScreen from "./src/screens/SplashScreen";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <SplashScreen /> <StatusBar style="auto" />
     </View>
   );
 }
@@ -14,8 +14,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
   },
 });
---
+++ src/screens/SplashScreen.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const SplashScreen = () => {
+  return (
+    <View className="flex-1 items-center justify-center bg-black text-white">
+      <Text>SplashScreen</Text>
+    </View>
+  );
+};
+
+export default SplashScreen;
