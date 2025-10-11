91a98449430350e155ee074dbf701b8f11c105fc
khaymanii <kennethakpo61@gmail.com>
2025-10-11 09:30:46 +0100

    fix

--- src/screens/SplashScreen.tsx
+++ src/screens/SplashScreen.tsx
@@ -1,9 +1,16 @@
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 
 const SplashScreen = () => {
   return (
-    <View className="flex-1 items-center justify-center bg-black text-white">
-      <Text>SplashScreen</Text>
+    <View className="flex-1 items-center justify-center bg-black">
+      <Image
+        source={require("../../assets/logo.png")}
+        className="w-6 h-6"
+        resizeMode="cover"
+      />
+      <Text className="text-white text-3xl font-bold">
+        Founder<Text className="text-purple-800">IQ</Text>
+      </Text>
     </View>
   );
 };
